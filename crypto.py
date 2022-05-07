ebd53c1d32c0e58227105efb92d80bd6f74b0fee
shivansh <shivansh@cognitivegarage.ai>
2022-05-07 09:46:11 +0530

    self locking added

--- crypto.py
+++ crypto.py
@@ -1,10 +1,12 @@
 import base64
 import json
 import os
-
+from threading import Thread
+import time
+import cryptography
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet, InvalidToken
 
 
 # Returns a string representation of a salt
@@ -19,7 +21,10 @@ def generate_salt(size=16):
     """
     return base64.b64encode(os.urandom(size)).decode()
 
+class InvalidKeyException(Exception):
+    pass
 
+# TODO add functionality to re-encrypt data after some timeout time
 class crypto:
     """Cryptography class for Encrypting and Decrypting text
     """
@@ -35,11 +40,14 @@ class crypto:
         self.data = str(data)
         self.salt = salt
         self.pass_phrase = pass_phrase
+        self.die=False
         
-        # self.encrypt_status = False
-
-        self.key = self.create_key()
-
+        self.valid_key = True
+        self.__max_counter = 20
+        self.counter = self.__max_counter
+        self.pass_watcher_thread = Thread(target=self.countdown)
+        self.pass_watcher_thread.start()
+        self.refresh_counter()
 
     def create_key(self):
         """creates key encryption
@@ -67,10 +75,13 @@ class crypto:
         """
         # if self.encrypt_status == True:
         #     return
+        if not self.valid_key:
+            raise InvalidKeyException("Invalid PassPhrase")
 
-        f = Fernet(self.key)
-
+        key = self.create_key()
+        f = Fernet(key)
         self.data = f.encrypt(self.data.encode()).decode()
+        # self.encrypt_level+=1
         # self.encrypt_status = True
         return self
 
@@ -79,12 +90,53 @@ class crypto:
         """
         # if self.encrypt_status == False:
         #     return
+        if not self.valid_key:
+            raise InvalidKeyException("Invalid PassPhrase")
+        # if self.encrypt_level <= 0:
+        #     return self
+        
+        key = self.create_key()
 
-        f = Fernet(self.key)
-
-        self.data = f.decrypt(self.data.encode()).decode()
+        try:
+            f = Fernet(key)
+            self.data = f.decrypt(self.data.encode()).decode()
+        except InvalidToken:
+            return self 
+        except:
+            raise InvalidKeyException("Invalid PassPhrase")
+        # self.encrypt_level-=1
         # self.encrypt_status = False
         return self
+
+    def countdown(self):
+        """Will run in separate thread and will after some time, reset the pass_phrase and encrypt the data
+        """
+        while self.counter > 0:
+            time.sleep(1)
+            self.counter-=1
+            if self.die == True:
+                print("DIEE")
+                return
+        self.encrypt()
+        self.valid_key=False
+        self.pass_phrase = ""
+            
+    def update_pass_phrase(self, pass_phrase):
+        """Will run after we have run down our countdown and we wish to again use the data, need to re enter password for encryption or decryption
+
+        Args:
+            pass_phrase (str): the password for encryption or decryption of the data
+        """
+        self.pass_phrase = pass_phrase
+        self.valid_key = True
+        self.refresh_counter()
+
+    def refresh_counter(self):
+        self.counter = self.__max_counter
+        if self.pass_watcher_thread.is_alive():
+            return
+        self.pass_watcher_thread = Thread(target=self.countdown)
+        self.pass_watcher_thread.start()
 
     def __str__(self):
         return json.dumps(self.to_dict())
@@ -94,3 +146,6 @@ class crypto:
             'salt':self.salt,
             'data':self.data
         }
+
+    def clean(self):
+        self.die=True
